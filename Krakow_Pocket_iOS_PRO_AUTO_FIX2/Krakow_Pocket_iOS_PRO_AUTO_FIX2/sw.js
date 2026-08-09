04031b0d709940ce72c021e9e2618e33c21a0c21
ismaelalvaradocontacto-del <ismaelalvarado.contacto@gmail.com>
2026-08-10 01:49:09 +0200

    Refresh app cache for typography tweak

--- Krakow_Pocket_iOS_PRO_AUTO_FIX2/Krakow_Pocket_iOS_PRO_AUTO_FIX2/sw.js
+++ Krakow_Pocket_iOS_PRO_AUTO_FIX2/Krakow_Pocket_iOS_PRO_AUTO_FIX2/sw.js
@@ -1,4 +1,4 @@
-const CACHE="krakow-pocket-v3-20260810";
+const CACHE="krakow-pocket-v3-20260810b";
 const CORE=["./","./index.html","./styles.css","./data.js","./app.js","./manifest.webmanifest","./icon-192.svg","./icon-512.svg"];
 self.addEventListener("install",event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(CORE)))});
 self.addEventListener("activate",event=>{event.waitUntil(Promise.all([self.clients.claim(),caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))]))});
