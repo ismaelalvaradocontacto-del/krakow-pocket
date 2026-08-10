aec48e1d7053f57bc78eb634501a14f40d6f5c6e
ismaelalvaradocontacto-del <ismaelalvarado.contacto@gmail.com>
2026-08-10 10:00:48 +0200

    Cache RPG v3.8 assets and force fresh deploy

--- Krakow_Pocket_iOS_PRO_AUTO_FIX2/Krakow_Pocket_iOS_PRO_AUTO_FIX2/sw.js
+++ Krakow_Pocket_iOS_PRO_AUTO_FIX2/Krakow_Pocket_iOS_PRO_AUTO_FIX2/sw.js
@@ -1,5 +1,5 @@
-const CACHE="krakow-pocket-v3-20260810s";
-const CORE=["./","./index.html","./styles.css","./data.js","./app.js","./enhancements.js","./enhancements.css","./trip-tools.js","./trip-tools-core.js","./trip-tools.css","./v34.js","./v34.css","./v35.js","./v35.css","./v36.js","./v36.css","./manifest.webmanifest","./icon-192.svg","./icon-512.svg"];
+const CACHE="krakow-pocket-v3-20260810t";
+const CORE=["./","./index.html","./styles.css","./data.js","./app.js","./enhancements.js","./enhancements.css","./trip-tools.js","./trip-tools-core.js","./trip-tools.css","./v34.js","./v34.css","./v35.js","./v35.css","./v36.js","./v36.css","./v37.js","./v37.css","./v38.js","./v38.css","./manifest.webmanifest","./icon-192.svg","./icon-512.svg"];
 const SUPABASE_HOST="ahzmwkztlakejmrvgcdm.supabase.co";
 const SUPABASE_KEY="sb_publishable_sf-RddHTp5jdFCQOfRBBsQ_PZGKOlxJ";
 self.addEventListener("install",event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(CORE)))});
