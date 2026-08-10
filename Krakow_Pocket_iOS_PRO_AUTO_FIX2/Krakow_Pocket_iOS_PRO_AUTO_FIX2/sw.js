c03dc4609d7718b09d2d22ee0940e61c7ef58ec4
ismaelalvaradocontacto-del <ismaelalvarado.contacto@gmail.com>
2026-08-10 08:49:32 +0200

    Refresh cache for transient notification fix

--- Krakow_Pocket_iOS_PRO_AUTO_FIX2/Krakow_Pocket_iOS_PRO_AUTO_FIX2/sw.js
+++ Krakow_Pocket_iOS_PRO_AUTO_FIX2/Krakow_Pocket_iOS_PRO_AUTO_FIX2/sw.js
@@ -1,4 +1,4 @@
-const CACHE="krakow-pocket-v3-20260810j";
+const CACHE="krakow-pocket-v3-20260810k";
 const CORE=["./","./index.html","./styles.css","./data.js","./app.js","./enhancements.js","./enhancements.css","./trip-tools.js","./trip-tools-core.js","./trip-tools.css","./v34.js","./v34.css","./manifest.webmanifest","./icon-192.svg","./icon-512.svg"];
 const SUPABASE_HOST="ahzmwkztlakejmrvgcdm.supabase.co";
 const SUPABASE_KEY="sb_publishable_sf-RddHTp5jdFCQOfRBBsQ_PZGKOlxJ";
