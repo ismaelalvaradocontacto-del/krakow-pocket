62a08d80bd174551044be5cbaf96d54d5613c82b
ismaelalvaradocontacto-del <ismaelalvarado.contacto@gmail.com>
2026-08-10 14:09:47 +0200

    Publish v6.4 style audit cache

--- Krakow_Pocket_iOS_PRO_AUTO_FIX2/Krakow_Pocket_iOS_PRO_AUTO_FIX2/sw.js
+++ Krakow_Pocket_iOS_PRO_AUTO_FIX2/Krakow_Pocket_iOS_PRO_AUTO_FIX2/sw.js
@@ -1,4 +1,4 @@
-const CACHE="krakow-pocket-v6-3-20260810b";
+const CACHE="krakow-pocket-v6-4-20260810a";
 const CORE=["./","./index.html","./styles.css","./data.js","./app.js","./runtime.js","./enhancements.js","./enhancements.css","./game.js","./game.css","./visuals.js","./visuals.css","./assets/game-art.svg","./assets/characters.svg","./assets/village.svg","./assets/world-map.svg","./manifest.webmanifest","./icon-192.svg","./icon-512.svg"];
 const SUPABASE_HOST="ahzmwkztlakejmrvgcdm.supabase.co";
 const SUPABASE_KEY="sb_publishable_sf-RddHTp5jdFCQOfRBBsQ_PZGKOIxJ";
