cbe8ad0b12eee610958689a2af08b178a1b71917
ismaelalvaradocontacto-del <ismaelalvarado.contacto@gmail.com>
2026-08-10 22:21:55 +0200

    Cache navigation-safe interaction synchronization

--- Krakow_Pocket_iOS_PRO_AUTO_FIX2/Krakow_Pocket_iOS_PRO_AUTO_FIX2/sw.js
+++ Krakow_Pocket_iOS_PRO_AUTO_FIX2/Krakow_Pocket_iOS_PRO_AUTO_FIX2/sw.js
@@ -1,4 +1,4 @@
-const CACHE="krakow-pocket-v16-player-stability-20260810o";
+const CACHE="krakow-pocket-v17-navigation-safe-sync-20260810p";
 const CORE=["./","./index.html","./styles.css","./game.css","./storybook.css","./compat.css","./profiles.css","./data.js","./compat.js","./state-bridge.js","./network-status.js","./player-stability.js","./interaction-fix.js","./mission-fix.js","./celebration-guard.js","./celebration-stability.js","./portrait-stability.js","./runtime.js","./stability.js","./app.js","./enhancements.js","./game.js","./visuals.js","./assets/game-art.svg","./assets/characters.svg","./assets/village.svg","./assets/world-map.svg","./manifest.webmanifest","./icon-192.svg","./icon-512.svg"];
 self.addEventListener("install",e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)))});
 self.addEventListener("activate",e=>{e.waitUntil(Promise.all([self.clients.claim(),caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))]))});
