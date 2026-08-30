8801a6137cd53209b3145e7019d4d87527ba90a1
ismaelalvaradocontacto-del <ismaelalvarado.contacto@gmail.com>
2026-08-30 07:07:33 +0200

    Add Pocket page registry

---
+++ Krakow_Pocket_iOS_PRO_AUTO_FIX2/Krakow_Pocket_iOS_PRO_AUTO_FIX2/pages.js
@@ -0,0 +1,11 @@
+window.POCKET_PAGES = [
+  {
+    id: "krakow-pocket",
+    title: "Kraków Pocket",
+    description: "La aventura de Cracovia: mapa, encargos, presupuesto, recuerdos y crónica.",
+    category: "Viajes",
+    icon: "🐉",
+    href: "./krakow-pocket.html",
+    createdAt: "2026-08-08T12:00:00+02:00"
+  }
+];
